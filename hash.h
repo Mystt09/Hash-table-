4f6db46a6216e378ad05be1129e09f0442133725
Mystt09 <112113231+Mystt09@users.noreply.github.com>
2025-03-02 12:35:44 -0700

    Update hash.h
    updated hash.h

--- hash.h
+++ hash.h
@@ -1,8 +1,106 @@
 #include <iostream>
 #include <string>
-#include <cmath>
-// You are free to use additional libraries as long as it's not PROHIBITED per instruction.
+#include "hash.h"
 
 using namespace std;
 
-int hash_function(string text);
+
+HashTable::HashTable(int sizeK) {
+
+    tableSize = sizeK; 
+
+    table = new item*[tableSize]; 
+
+    for (int i = 0; i < tableSize; i++) {
+
+        table[i] = nullptr;
+
+    } 
+}
+     
+// Destructor: Free memory
+HashTable::~HashTable() {
+
+    for (int i = 0; i < tableSize; i++) {
+        item* temp = table[i];
+
+        while (temp) {
+
+            item* next = temp->next;
+            delete temp;
+            temp = next;
+
+        }
+
+    }
+    delete[] table; // Free the array
+}
+    
+void HashTable::insertItem(string text) {
+
+    int index = hashFunction(text); 
+
+    item* newItem = new item(text);
+    newItem->next = table[index];
+    table[index] = newItem;
+
+       }
+
+       //void removeItem(int key); 
+       //string searchTable(int key); 
+
+void HashTable::printTable(){
+
+    for (int i = 0; i < tableSize; i++) {
+        printf("Slot %d: ", i);  // Print slot index
+
+        item* temp = table[i];
+        while (temp) {
+            printf("%d -> ", temp->key);  // Print the key
+            temp = temp->next;
+        }
+        printf("NULL\n");  // End of chain
+    }
+
+
+       }
+
+
+int HashTable::hashFunction(string text) {
+    // Implement your own hash function here
+
+    // get the first char of the letter 
+    char letter = text[0];  // stores the first char 
+
+    int ascii = (int)letter; // gets the ascii of that char 
+
+    int lowerCaseA = (int)'a';
+    int upperCaseA = (int)'A';
+
+    int lowerCaseZ = (int) 'z';
+    int upperCaseZ = (int) 'Z'; 
+
+    //  x > 97 and x < 123 subtract by ascii of 'a' -> 97
+    // x > 65 x < 91 subtract by ascii of 'A' -> 65
+
+    if (ascii > lowerCaseA && ascii < lowerCaseZ ) { // checks if the first letter is lower case 
+
+        // subtract the ascii by ascii of 'a' 
+
+        int hashValue = ascii - lowerCaseA; 
+
+        return hashValue % tableSize;
+
+    }
+
+    if (ascii > upperCaseA && ascii < upperCaseZ ) { // checks if the first letter is upper case 
+
+        int hashValue = ascii - upperCaseA; 
+
+        return hashValue % tableSize;
+
+    }
+    // check if the ascii is upper or lower case 
+
+    return 1;
+}
